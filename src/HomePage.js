354b380368474bd24abe227b21db58aaffeb6b96
emin-arslan <eminarslan1864@gmail.com>
2023-05-16 01:15:58 +0300

    Initial commit

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,17 +1,19 @@
 import React from "react";
 import pic from "./assets/export.png";
+import { Link } from "react-router-dom";
+
 
 const HomePage = () => {
   return (
-    <div className="w-11/12 flex items-center  pt-16  min-w-[435px] ">
-      <div className="w-6/12 space-y-5  min-w-[435px]   ">
-        <div className="w-11/12 sm:w-full md:w-full flex justify-center lg:justify-base">
-          <span className="w-6/12 flex md:w-full md:justify-center   sm:w-full sm:justify-center   text-4xl font-thin md:text-3xl lg:text-2xl xl:text-3xl sm:text-3xl  text-orange-800 ">
+    <div className="w-11/12 flex items-center  mt-16 sm:pt-5 min-w-[435px] sm:mt-0 ">
+      <div className="w-6/12 space-y-5  min-w-[435px]  md:min-w-[300px]  md:justify-start ml-3   ">
+        <div className="w-11/12 sm:w-full md:w-full flex justify-center  lg:justify-base ">
+          <span className="w-6/12 flex md:w-full md:justify-start   sm:w-full sm:justify-center   text-4xl font-thin md:text-3xl lg:text-2xl xl:text-3xl sm:text-3xl  text-orange-800 ">
             Welcome to your <br /> professional society!
           </span>
         </div>
 
-        <div className="w-11/12 sm:w-full md:w-full flex justify-center">
+        <div className="w-11/12 sm:w-full md:w-full md:justify-start  flex justify-center">
           <div className="w-6/12 sm:w-8/12  md:w-7/12">
             <div className="w-full ">
               <label htmlFor="idValue" className="text-sm text-gray-500 ">
@@ -28,8 +30,8 @@ const HomePage = () => {
           </div>
         </div>
 
-        <div className="w-11/12 sm:w-full md:w-full flex justify-center">
-          <div className="w-6/12  sm:w-8/12 md:w-7/12">
+        <div className="w-11/12 sm:w-full md:w-full md:justify-start    flex justify-center">
+          <div className="w-6/12  sm:w-8/12 md:w-7/12 lg:f">
             <div className="w-full ">
               <label htmlFor="idValue" className="text-sm text-gray-500">
                 Password
@@ -46,7 +48,7 @@ const HomePage = () => {
           </div>
         </div>
 
-        <div className="w-11/12 sm:w-full md:w-full flex justify-center ">
+        <div className="w-11/12 sm:w-full md:w-full flex md:justify-start  justify-center ">
           <div className="w-6/12 sm:w-8/12 md:w-7/12">
             <span className="text-l underline orange hover:text-orange-700 hover:cursor-pointer">
               Forgetten Password ?
@@ -54,33 +56,33 @@ const HomePage = () => {
           </div>
         </div>
 
-        <div className="w-11/12  sm:w-full md:w-full  flex justify-center ">
+        <div className="w-11/12  sm:w-full md:w-full md:justify-start  flex justify-center ">
           <div className=" hover:cursor-pointer  text-white flex items-center hover:bg-orange-400 justify-center  w-6/12 sm:w-8/12  md:w-7/12 bg-orange-500 rounded-xl h-10">
             <span className=" font-bold">Login</span>
           </div>
         </div>
 
-        <div className="w-11/12 sm:w-full md:w-full flex justify-center ">
+        <div className="w-11/12 sm:w-full md:w-full md:justify-start  flex justify-center ">
           <div className="h-5 border-b border-orange-400 text-2xl sm:w-8/12 md:w-7/12 w-6/12 text-center">
             <span className="px-5 text-gray-400  text-base bg-white">or</span>
           </div>
         </div>
 
-        <div className="w-11/12 sm:w-full md:w-full flex justify-center ">
+        <div className="w-11/12 sm:w-full md:w-full md:justify-start  flex justify-center ">
         <div className=" hover:cursor-pointer  flex items-center hover:bg-orange-100 justify-center  w-6/12 sm:w-8/12 md:w-7/12  border border-orange-500 rounded-xl h-10">
               <span className="text-orange-800 ">Continue with Google</span>
         </div>
         </div>
-
-        <div className="w-11/12  sm:w-full md:w-full flex justify-center ">
+        <Link to="/signup" >
+        <div className="w-11/12 mt-3 sm:w-full md:w-full flex  md:justify-start  justify-center ">
         <div className=" hover:cursor-pointer  flex items-center hover:bg-orange-100 justify-center  w-6/12  sm:w-8/12 md:w-7/12 border border-orange-500 rounded-xl h-10">
               <span className="text-orange-800 ">Are you new? Join us Now!</span>
         </div>
-        </div>
+        </div></Link>
 
       </div>
-      <div className="w-6/12  sm:min-w-[400px]">
-      <img src={pic}  className=" w-[502px] min-w-[402px] sm:hidden " alt="sa" />
+      <div className="w-6/12  sm:min-w-[400px]  flex justify-center">
+      <img src={pic}  className=" w-[502px] min-w-[402px] h-[500px] sm:hidden " alt="sa" />
       </div>
     </div>
   );
--- src/Navi.js
+++ src/Navi.js
@@ -4,16 +4,16 @@ import { Link, useLocation} from "react-router-dom";
 const Navi = () => {
 const location = useLocation();  
   return (
-    <div className="w-11/12 h-20 flex items-center text-[#707070] font-exo ">
+    <div className="w-11/12 h-20 flex items-center text-[#707070] space-x-2 font-exo  md:w-full ">
 
-      <div className="w-4/12 centered-items">
+      <div className="w-4/12 sm:w-full centered-items">
         <Link to='/'>
-          <span className="orange font-bold text-2xl font-sans">Apply4Job</span>
+          <span className="orange font-bold text-2xl font-sans ">Apply4Job</span>
         </Link>
       </div>
       
-      <div className={`w-6/12 flex justify-base ${location.pathname === ('/login') || location.pathname ===('/signup')  ? 'hidden':'' }`}>
-        <ul className="flex space-x-10 font-bold">
+      <div className={`w-4/12 xl:w-5/12 lg:w-8/12 md:w-full  sm:hidden flex justify-base ${location.pathname === ('/login') || location.pathname ===('/signup')  ? 'hidden':'' }`}>
+        <ul className="flex space-x-10 sm:space-x-1 font-bold">
           <li className="hover:cursor-pointer hover:text-blue-400">Find Job</li>
           <li className="hover:cursor-pointer hover:text-blue-400">
             Company Application
@@ -24,7 +24,7 @@ const location = useLocation();
         </ul>
       </div>
 
-      <div className={`w-2/12 h-10 space-x-5 centered-items ${location.pathname === ('/login') || location.pathname ===('/signup')  ? 'hidden':'' }  `}>
+      <div className={`w-4/12 md:w-4/12  sm:hidden h-10 space-x-5 centered-items ${location.pathname === ('/login') || location.pathname ===('/signup')  ? 'hidden':'' }  `}>
         <Link to="/signup">
           <span className="font-bold border-r h-full centered-items pr-4 hover:orange hover:cursor-pointer">
             Sign Up
